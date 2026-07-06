eb25eec153bea104e73cdd9fcb7db902762e666a
ilsa54noor-alt <ilsa.54.noor@gmail.com>
2026-07-06 16:51:02 +0500

    Add number guessing game in guessing_game.c
    Implements a number guessing game where the user guesses a randomly generated number between 1 and 100. The game provides feedback on whether the guess is too high or too low and counts the number of guesses.

---
+++ guessing_game.c
@@ -0,0 +1,36 @@
+// Number Guessing Game — generates a random number, user guesses until correct
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main()
+{
+    printf("NUMBER GUESSING GAME\n");
+    printf("Guess a number between 1 and 100\n");
+    srand(time(0));
+    int random_number = (rand()%100) + 1;
+    int guessed_number;
+    int no_of_guesses = 0;
+
+    do
+    {
+        printf("Enter your guess=\n");
+        scanf("%d",&guessed_number);
+        no_of_guesses++;
+
+        if(guessed_number<random_number){
+            printf("Too low — guess higher\n");
+        }
+        else if(guessed_number>random_number){
+            printf("Too high — guess lower\n");
+        }
+        else{
+            printf("congrats🎉🎉\n");
+        }
+    }
+    while(guessed_number != random_number);
+
+    printf("Number of guesses =%d\n",no_of_guesses);
+
+    return 0;
+}
